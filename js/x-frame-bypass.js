f5dbdd470dddb55ff10620f10e7237490e40de0b
Franco (nextime) Lanza <nextime@nexlab.it>
2022-07-09 02:06:30 +0000

    Fix license headers

--- js/x-frame-bypass.js
+++ js/x-frame-bypass.js
@@ -1,3 +1,23 @@
+/**
+ * Copyright: 2022 (c)Franco (nextime) Lanza <franco@nexlab.it>
+ * License: GNU/GPL version 3.0
+ *
+ * This file is part of SexHackMe Wordpress Plugin.
+ *
+ * SexHackMe Wordpress Plugin is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, 
+ * or (at your option) any later version.
+ *
+ * SexHackMe Wordpress Plugin is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with SexHackMe Wordpress Plugin. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 customElements.define('x-frame-bypass', class extends HTMLIFrameElement {
 	constructor () {
 		super()
--- template/video.php
+++ template/video.php
@@ -1,10 +1,22 @@
 <?php
 /**
- * The template for displaying archive pages.
+ * Copyright: 2022 (c)Franco (nextime) Lanza <franco@nexlab.it>
+ * License: GNU/GPL version 3.0
  *
- * Learn more: https://developer.wordpress.org/themes/basics/template-hierarchy/
+ * This file is part of SexHackMe Wordpress Plugin.
  *
- * @package storefront
+ * SexHackMe Wordpress Plugin is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, 
+ * or (at your option) any later version.
+ *
+ * SexHackMe Wordpress Plugin is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with SexHackMe Wordpress Plugin. If not, see <https://www.gnu.org/licenses/>.
  */
 
 get_header(); ?>
